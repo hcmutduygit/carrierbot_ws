1191a4aa048d3040cf096f72b943880d039d7b2d
hcmutduygit <quangduy160204@gmail.com>
2026-04-19 15:33:37 +0700

    test pure pursuit

--- src/carrierbot_bringup/launch/real_robot.launch.py
+++ src/carrierbot_bringup/launch/real_robot.launch.py
@@ -163,6 +163,6 @@ def generate_launch_description():
         laser_filter,
         TimerAction(period=2.0, actions=[controller]),
         TimerAction(period=4.0, actions=[slam]),
-        TimerAction(period=15.0, actions=[navigation]),
-        TimerAction(period=16.0, actions=[rviz]),
+        TimerAction(period=6.0, actions=[navigation]),
+        TimerAction(period=8.0, actions=[rviz]),
     ])
